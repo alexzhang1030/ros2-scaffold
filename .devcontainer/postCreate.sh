0bd65796739adae0babaab4061a3e2909fd70388
Alex Zhang <alexzhang1030@foxmail.com>
2025-06-17 13:44:46 +0000

    chore: sync pixi lockfile

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-mkdir -p src
-sudo rosdep fix-permissions
-rosdep update
-rosdep install --from-paths /home/ws/src --ignore-src -y
-sudo chown -R $(whoami) /home/ws/
-colcon build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+pixi install
